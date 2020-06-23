9ca4355ce4d8fbb51d13675a74cc6822fb544788
justineprovost <45413010+justineprovost@users.noreply.github.com>
2020-06-23 18:16:33 -0400

    Add files via upload

---
+++ final_report/js/saf_icons.js
@@ -0,0 +1,7 @@
+var animIcons = lottie.loadAnimation({
+  container: document.getElementById('saficons'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'animation/data.json' // the path to the animation json
+});
